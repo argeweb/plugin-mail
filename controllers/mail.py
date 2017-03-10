deff62679c30a1f4d862b80f75fd8260f6b3eef8
cwen0708 <cwen0708@gmail.com>
2017-03-10 21:46:07 +0800

    訂購通知信

--- controllers/mail.py
+++ controllers/mail.py
@@ -50,4 +50,26 @@ class Mail(Controller):
 基於安全理由，密碼重設連結將在重設密碼後失效。</p>
 <p>--<br /> 此通知為系統自動發送，若您沒有使用『無法登入』功能，請忽略此信</p>
 ''')
+
+        m.replace_context('order_create_send_to_user', u'訂購通知信', u"{{ site_name }} 訂購通知信", u'''
+<p>親愛的 {{ purchaser_name }} 您好:</p>
+<p>感謝您訂購「{{ site_name }}」的商品／服務，為確保訂單權益，請將款項匯入以下轉入帳號；您越早完成繳費，我們就能越快為您寄送商品／啟動服務。</p>
+<p>您的訂單和轉帳資料如下：</p>
+<table style="color: #000000; font: 13px Verdana;" border="0" width="100%" cellspacing="2" cellpadding="4"><tbody>
+<tr><td><div align="center"><strong><span style="color: #535353;">訂單編號</span></strong></div></td><td>{{ order_no }}</td></tr>
+<tr><td><div align="center"><strong><span style="color: #535353;">訂購日期</span></strong></div></td><td>{{ created }}</td></tr>
+<tr><td><div align="center"><strong><span style="color: #535353;">付費方式</span></strong></div></td><td>{{ payment_type_title }}</td></tr>
+<tr><td><div align="center"><strong><span style="color: #535353;">付款狀態</span></strong></div></td><td>{{ payment_status_text }}</td></tr>
+<tr><td><div align="center"><strong><span style="color: #535353;">訂單明細</span></strong></div></td><td>{{ order_items }}</td></tr></tbody></table>''')
+
+        m.replace_context('order_create_send_to_admin', u'訂購通知信', u"{{ site_name }} 訂購通知信，來至", u'''
+<p>來至 {{ purchaser_name }} 的訂單:</p>
+<table style="color: #000000; font: 13px Verdana;" border="0" width="100%" cellspacing="2" cellpadding="4"><tbody>
+<tr><td><div align="center"><strong><span style="color: #535353;">訂單編號</span></strong></div></td><td>{{ order_no }}</td></tr>
+<tr><td><div align="center"><strong><span style="color: #535353;">訂購日期</span></strong></div></td><td>{{ created }}</td></tr>
+<tr><td><div align="center"><strong><span style="color: #535353;">總金額</span></strong></div></td><td>{{ total_amount }}</td></tr>
+<tr><td><div align="center"><strong><span style="color: #535353;">訂單備註</span></strong></div></td><td>{{ message }}</td></tr>
+<tr><td><div align="center"><strong><span style="color: #535353;">付費方式</span></strong></div></td><td>{{ payment_type_title }}  |  {{ payment_status_text }}</td></tr>
+<tr><td><div align="center"><strong><span style="color: #535353;">寄送方式</span></strong></div></td><td>{{ freight_type_title }}  |  {{ freight_status_text }}</td></tr>
+<tr><td><div align="center"><strong><span style="color: #535353;">訂單明細</span></strong></div></td><td>{{ order_items }}</td></tr></tbody></table>''')
         return u'已重置樣板'
